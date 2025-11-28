5c532c3ab31a8afebf50ce8ad5b9932d771ec11b
Maruf Quadri (Sophist) <240581357+upspacehq@users.noreply.github.com>
2025-11-29 00:05:12 +0100

    Create components/blog/CategoryBadge.js

---
+++ components/blog/CategoryBadge.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from '../../styles/PostCard.module.css';
+
+const CategoryBadge = ({ category }) => {
+  const slug = category.toLowerCase().replace(/\s+/g, '-');
+  
+  return (
+    <Link href={`/blog/category/${slug}`}>
+      <span className={styles.category}>{category}</span>
+    </Link>
+  );
+};
+
+export default CategoryBadge;
